afdf787ad1c9849ac720bba1ff52b103800f6545
Pascal <4441341+ict-cloud@users.noreply.github.com>
2025-02-01 21:21:21 +0100

    Fix initial release (#8)
    * Initial develop commit
    
    * SVG Preview is rendered as PNG (#1)
    
    * Initial save of png file
    
    * Cleanup render warnings
    
    * Add file dialog for save file
    
    * Fixes
    
    * adapt svg_result
    
    * add logging
    
    * Eliminate compile errors.
    Update Readme
    
    * Add tests
    
    * Add Github Actions
    
    * Save a sized png
    
    * Fix compiler warnings
    
    * small fixes
    
    * more modular png module
    
    * refactor main view
    
    * img based preview
    
    * Use prelude
    
    * scaling of image and editor view
    
    * empty svg will not produce a png error.
    
    * add tabview
    
    * try view size in button
    
    * Refactoring the preview
    
    * add width to parsing
    
    * png preview
    
    * move buttons to dedicated module
    
    * small corrections
    
    * Better approach
    
    * fixing update behaviour of editor
    
    * cleanup
    
    * editor displays full size if no preview is around
    
    * No more crashes when try to render errors
    
    * correct display of error in preview
    
    * add save button
    
    * Activate Dependabot on develop
    
    * Correct typo in README.MD
    
    * Gh cicd setup (#6)
    
    * Setup github actions
    
    * Release Workflow
    
    * Cargo fmt
    
    * Cargo Clippy Fixes
    
    * fix fmt
    
    * Fix mainview (#7)
    
    * Fullsize svg preview container
    
    * Fix compiler messages

--- src/parser/pikchr.rs
+++ src/parser/pikchr.rs
@@ -1,42 +1,44 @@
-use pikchr::{Pikchr, PikchrFlags};
 use crate::img::png;
+use pikchr::{Pikchr, PikchrFlags};
 use xml::escape::escape_str_pcdata;
 
-
 pub fn pik_svgstring(i_raw: &str, i_svg_old: &str) -> (String, String) {
-  let mut pik_err = String::from("");
-  let svg_str = match Pikchr::render(i_raw, None, PikchrFlags::default()) {
-      Ok(p) => p.rendered().to_owned(),
-      Err(e) => {pik_err = e.to_owned(); i_svg_old.to_owned()},
-  };
+    let mut pik_err = String::from("");
+    let svg_str = match Pikchr::render(i_raw, None, PikchrFlags::default()) {
+        Ok(p) => p.rendered().to_owned(),
+        Err(e) => {
+            pik_err = e.to_owned();
+            i_svg_old.to_owned()
+        }
+    };
 
-  (svg_str, pik_err)
+    (svg_str, pik_err)
 }
 
 pub fn pik_preview_width(i_rawstr: &str, i_width: f64) -> Vec<u8> {
-  let flags = PikchrFlags::default();
-  //flags.generate_html_errors();
-  let pikrendr = Pikchr::render(i_rawstr, None, flags);
-  let svgstr = match pikrendr {
-    Ok(p) => p.rendered().to_owned(),
-    // this will not display as proper svg
-    Err(e) => {
-      if !e.starts_with("<!-- empty pikchr diagram -->") {
-        let xml_e = escape_str_pcdata(&e);
-        let lstr = xml_e.lines().collect::<Vec<&str>>().join(r#"</tspan><tspan dy="1.2em" x="10" dx="1em">"#);
-        format!(
-          r#"<svg xmlns="http://www.w3.org/2000/svg"><text><tspan dy="1.2em" x="10" dx="1em">{}</tspan></text></svg>"#, lstr)
-      } else {
-        e.as_str().to_owned()
-      }
-    }
-  };
-  println!("svgstring before image processing {}", svgstr);
-  let prev_width = if i_width > 0.0 {
-    Some(i_width)
-  } else {
-    None
-  };
-  let img = png::svg_to_png(&svgstr, prev_width);
-  img
+    let flags = PikchrFlags::default();
+    //flags.generate_html_errors();
+    let pikrendr = Pikchr::render(i_rawstr, None, flags);
+    let svgstr = match pikrendr {
+        Ok(p) => p.rendered().to_owned(),
+        // this will not display as proper svg
+        Err(e) => {
+            if !e.starts_with("<!-- empty pikchr diagram -->") {
+                let xml_e = escape_str_pcdata(&e);
+                let lstr = xml_e
+                    .lines()
+                    .collect::<Vec<&str>>()
+                    .join(r#"</tspan><tspan dy="1.2em" x="10" dx="1em">"#);
+                format!(
+                    r#"<svg xmlns="http://www.w3.org/2000/svg"><text><tspan dy="1.2em" x="10" dx="1em">{}</tspan></text></svg>"#,
+                    lstr
+                )
+            } else {
+                e.as_str().to_owned()
+            }
+        }
+    };
+    log::debug!("svgstring before image processing {}", svgstr);
+    let prev_width = if i_width > 0.0 { Some(i_width) } else { None };
+    png::svg_to_png(&svgstr, prev_width)
 }
