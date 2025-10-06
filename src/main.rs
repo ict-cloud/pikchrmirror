2dc189c1ed0cc9563ef5d356b7797d9e57875d7a
Pascal <master@ict-cloud.ch>
2025-10-06 20:55:41 +0200

    Add font for symbols

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ mod img;
 mod parser;
 
 use app::MirrorApp;
+use iced::Font;
 
 #[cfg(test)]
 mod tests;
 
 pub fn main() -> iced::Result {
-    iced::application(MirrorApp::title, MirrorApp::update, MirrorApp::view).run()
+    iced::application(MirrorApp::title, MirrorApp::update, MirrorApp::view)
+        .font(include_bytes!("../fonts/icons.ttf").as_slice())
+        .default_font(Font::MONOSPACE)
+        .run()
 }
